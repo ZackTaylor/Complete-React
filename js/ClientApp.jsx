3bbe0af047ad8b6dd56028bb8f8f444f6ea9af64
Zack Tylor <taylor.zackory@gmail.com>
2017-10-17 18:51:52 -0500

    use / befor public imports and create home page using jsx

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>svideo</h1>
+      <input type="text" placeholder="Search" />
+      <a> or Browse All</a>
+    </div>
+  </div>
+);
 
-const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
-
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    null,
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'House Hunters', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'Lime' })
-  );
-};
-
-render(ce(MyFirstComponent), document.getElementById('app'));
+render(<App />, document.getElementById('app'));
