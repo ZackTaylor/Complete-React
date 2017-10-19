bdd8c39a0d8e88f98cb16ba30a05aba7b99fd79e
Zack Tylor <taylor.zackory@gmail.com>
2017-10-18 19:12:14 -0500

    update search component to filter results

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,3 +1,5 @@
+// @flow
+
 import React, { Component } from 'react';
 import ShowCard from './ShowCard';
 import preload from '../data.json';
@@ -6,7 +8,7 @@ class Search extends Component {
   state = {
     searchTerm: ''
   };
-  handleSearchTermChange = event => {
+  handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
     this.setState({ searchTerm: event.target.value });
   };
   render() {
@@ -21,7 +23,14 @@ class Search extends Component {
             placeholder="Search"
           />
         </header>
-        <div>{preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}</div>
+        <div>
+          {preload.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
       </div>
     );
   }
